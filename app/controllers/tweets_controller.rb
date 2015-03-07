dd6e66eeb69e92b0cb91c90d0f47bc1b54d4e2e0
lemando <alejandro@lemando.com>
2015-03-07 15:19:24 -0500

    message created, break 3:17pm

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -6,6 +6,7 @@ class TweetsController < ApplicationController
 	def create
 		@tweet = Tweet.new(tweet_params)
 		@tweet.save
+			flash[:success] = "You have created a tweet"
 		render 'new'
 	end
 
