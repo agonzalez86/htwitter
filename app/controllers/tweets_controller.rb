847a7cfe61988b0adeb1cabeef15edb23b858317
lemando <alejandro@lemando.com>
2015-03-08 11:24:07 -0400

    sunday morning

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,6 @@
 class TweetsController < ApplicationController
 	before_action :authenticate_user!
-	
+
 	def new
 		@tweet = Tweet.new
 	end
@@ -8,9 +8,12 @@ class TweetsController < ApplicationController
 	def create
 		@tweet = Tweet.new(tweet_params)
 		@tweet.user = current_user
-		@tweet.save
+		if @tweet.save
 			flash[:success] = "You have created a tweet"
 			redirect_to new_tweet_path
+        else
+        	render 'new'
+        end
 	end
 
 	def index
