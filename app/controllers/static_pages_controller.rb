84d9c73baf855f3318d01e5fa50c98075d800cc6
lemando <alejandro@lemando.com>
2015-03-08 12:20:19 -0400

    User profiles created

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,12 @@
 class StaticPagesController < ApplicationController
-
+before_action :authenticate_user!
 	def main_page
+		if user_signed_in?
+			if current_user.profile
+				redirect_to new_tweet_path
+			else
+				redirect_to new_profile_path
+			end
+		end
 	end
 end
